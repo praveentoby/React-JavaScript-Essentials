7de46d4c34102d2139d5b4b7fbc61c0e3f3ff1a8
praveen.srinivasa <praveen.srinivasa-ext@believe.com>
2025-03-18 13:53:36 +0530

    #1 - Destructuring basic tutorial

--- 01-DestructuringObjectsAndArray.js
+++ 01-DestructuringObjectsAndArray.js
@@ -1,13 +1,31 @@
-import * as data from  './data.json';
+import data from './data.json';
 
 function getBooks() {
-    return data;
+  return data;
 }
 
 function getBook(id) {
-    return data.find((d) => d.id === id)
+  return data.find((d) => d.id === id);
 }
 
-const books = getBook();
+const book = getBook(2);
 
-console.log("Books : ", books)
+// const title = book.title;
+// const author = book.author;
+
+const { title, author, pages, publicationDate, genres } = book;
+
+console.log('title :>> ', title);
+console.log('author :>> ', author);
+console.log('pages :>> ', pages);
+console.log('publicationDate :>> ', publicationDate);
+
+console.log('genres :>> ', genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+const [primaryGenre, secondaryGenre] = genres;
+
+console.log('primaryGenre :>> ', primaryGenre);
+console.log('secondaryGenre :>> ', secondaryGenre);
